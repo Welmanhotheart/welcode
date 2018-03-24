f9178af3c47785a2dc9839fdeb421641d0f4d675
Welmanhotheart <a274936580@163.com>
2018-03-24 21:28:56 +0800

    how to infere the common type

--- welcode/src/testcase/generics/GenericVarargs.java
+++ welcode/src/testcase/generics/GenericVarargs.java
@@ -1,5 +1,9 @@
+package testcase.generics;
+
 //: generics/GenericVarargs.java
 import java.util.*;
+
+import testcase.typeinfo.pets.*;
 
 public class GenericVarargs {
   public static <T> List<T> makeList(T... args) {
@@ -15,6 +19,12 @@ public class GenericVarargs {
     System.out.println(ls);
     ls = makeList("ABCDEFFHIJKLMNOPQRSTUVWXYZ".split(""));
     System.out.println(ls);
+ /*
+  *  first they are autoboxed into their wrapped type and then their base class type are inferred,It's Number
+  *  Is this rule always true when applied to a hierarchy ?
+  */
+    System.out.println(makeList((byte)1, 2, 3f));
+    System.out.println(makeList(new Mutt(), new Rat(), 3f));// their commom ancestor is 'Comparable' interface
   }
 } /* Output:
 [A]
