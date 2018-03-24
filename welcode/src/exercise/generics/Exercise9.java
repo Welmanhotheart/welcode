7ef1bafa3cc7ae5def052d1b17e3783e06a89a99
Welmanhotheart <a274936580@163.com>
2018-03-24 20:55:28 +0800

    generic exercise9 is finished, byte-type parameter is wrapped into Byte type

---
+++ welcode/src/exercise/generics/Exercise9.java
@@ -0,0 +1,37 @@
+package exercise.generics;
+
+
+ class GenericMethods {
+public <V,T, E> void f(T x, V y, E z) {
+  System.out.println(x.getClass().getName());
+  System.out.println(y.getClass().getName());
+  System.out.println(z.getClass().getName());
+}
+public static void main(String[] args) {
+  GenericMethods gm = new GenericMethods();
+  gm.f("","", 1f);
+  gm.f('c',1L,1.0);
+  gm.f(1,(byte)1, gm);
+}
+} 
+
+public class Exercise9 {
+	public static void main(String[] args) {
+		GenericMethods.main(null);
+	}
+}
+
+/**
+ * output:
+ *java.lang.String
+	java.lang.String
+	java.lang.Float
+	
+	java.lang.Character
+	java.lang.Long
+	java.lang.Double
+	
+	java.lang.Integer
+	java.lang.Byte
+	exercise.generics.GenericMethods 
+ */
