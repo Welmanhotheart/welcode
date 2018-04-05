dda91485488862d98d8e13e152f692f430af03f2
Welmanhotheart <a274936580@163.com>
2018-04-05 16:14:57 +0800

    We can see that the compiler doesn't analyze the code to see if you perform any actual writes or reads, it only pay attention to the types of objects that are passed and returned.

--- welcode/src/testcase/generics/Holder.java
+++ welcode/src/testcase/generics/Holder.java
@@ -1,10 +1,12 @@
+package testcase.generics;
+
 //: generics/Holder.java
 
 public class Holder<T> {
   private T value;
   public Holder() {}
   public Holder(T val) { value = val; }
-  public void set(T val) { value = val; }
+  public void set(Object val) { value = (T) val; }
   public T get() { return value; }
   public boolean equals(Object obj) {
     return value.equals(obj);
@@ -20,11 +22,15 @@ public class Holder<T> {
     try {
       Orange c = (Orange)fruit.get(); // No warning
     } catch(Exception e) { System.out.println(e); }
-    // fruit.set(new Apple()); // Cannot call set()
-    // fruit.set(new Fruit()); // Cannot call set()
+//     fruit.set(new Apple()); // Cannot call set()
+//     fruit.set(new Fruit()); // Cannot call set(), yeah it's ok if the argument type of ‘set’ method is 'Object'
     System.out.println(fruit.equals(d)); // OK
   }
 } /* Output: (Sample)
 java.lang.ClassCastException: Apple cannot be cast to Orange
 true
 *///:~
+/**
+ * We can see that the compiler doesn't analyze the code to see if you perform any actual writes or reads,
+ * it only pay attention to the types of objects that are passed and returned.
+ */
