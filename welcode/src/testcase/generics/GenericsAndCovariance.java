2788c9be14836534e7d38416f359463ca788032b
Welmanhotheart <a274936580@163.com>
2018-04-05 16:07:50 +0800

    the compiler is not very smart...

--- welcode/src/testcase/generics/GenericsAndCovariance.java
+++ welcode/src/testcase/generics/GenericsAndCovariance.java
@@ -13,10 +13,7 @@ public class GenericsAndCovariance {
     // flist.add(new Object());
     flist.add(null); // Legal but uninteresting
     // We know that it returns at least Fruit:
-    List<? extends Number> nlst = new ArrayList<Integer>();
-    nlst.addAll(null);
-    String a = (String) new Object();
-    
+    flist.indexOf(new Apple());
   }
 } ///:~
 
@@ -24,4 +21,12 @@ public class GenericsAndCovariance {
  * The explanation about compile error when adding 'new Apple'、
  * the instance specify the type, but the reference which receive it doesn't specify the type
  * but in order to upcast to the reference ,that type is a 'don't actually care'
+ * And also the compiler is not very smart:
+ * By looking at the documentation for ArrayList, we find that the compiler is not that smart. 
+ * While add( ) takes an argument of the generic parameter type, contains( ) and indexOf( ) 
+ * take arguments of type Object. So when you specify an ArrayList <? extends Fruit >, the 
+ * argument for add( ) becomes’? extends Fruit’. From that description, the compiler cannot 
+ * know which specific subtype of Fruit is required there, so it won’t accept any type of Fruit. 
+ * It doesn’t matter if you upcast the Apple to a Fruit first—the compiler simply refuses to call 
+ * a method (such as add( )) if a wildcard is involved in the argument list. 
  */
