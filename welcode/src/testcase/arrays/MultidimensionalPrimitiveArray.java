1c479d8e68bf878eae20a92a5cd013c13d29d3db
Welmanhotheart <a274936580@163.com>
2018-04-22 20:12:15 +0800

    Arrays|MultidimensionalPrimitiveArray.java:Arrays.deepToString(a), its sourcecode : continue recursive going on

--- welcode/src/testcase/arrays/MultidimensionalPrimitiveArray.java
+++ welcode/src/testcase/arrays/MultidimensionalPrimitiveArray.java
@@ -1,3 +1,5 @@
+package testcase.arrays;
+
 //: arrays/MultidimensionalPrimitiveArray.java
 // Creating multidimensional arrays.
 import java.util.*;
@@ -8,8 +10,10 @@ public class MultidimensionalPrimitiveArray {
       { 1, 2, 3, },
       { 4, 5, 6, },
     };
-    System.out.println(Arrays.deepToString(a));
+    System.out.println(Arrays.deepToString(a));// continue recursive going on
+    System.out.println(Arrays.toString(a));
   }
 } /* Output:
 [[1, 2, 3], [4, 5, 6]]
+[[I@252f0999, [I@331f2ee1]
 *///:~
