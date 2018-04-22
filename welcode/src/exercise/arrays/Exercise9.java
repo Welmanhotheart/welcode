69cd530098adf4368ebcb4985e49cb93f48b17d1
Welmanhotheart <a274936580@163.com>
2018-04-22 21:15:57 +0800

    arrays|Exercise9.java:Cannot create a generic array of Peel<Banana>

---
+++ welcode/src/exercise/arrays/Exercise9.java
@@ -0,0 +1,19 @@
+package exercise.arrays;
+
+import exercise.arrays.BerylliumSphere;
+
+
+class Banana{}
+
+class Peel<T> {
+}
+
+
+public class Exercise9 {
+	public static void main(String[] args) {
+//		Peel<Banana>[] plb = new Peel<Banana>[10]; compile error:Cannot create a generic array of Peel<Banana>
+		@SuppressWarnings("unchecked")
+		Peel<Banana>[] plb = new Peel[10]; 
+		
+	}
+}
