62d930d737a9621b4f12aacbf02c547ac0529955
Welmanhotheart <a274936580@163.com>
2018-04-23 10:19:46 +0800

    Arrays|ComparingArrays.java: As for Arrrays.equals,there are many overloaded versions, if the parameters are  Object type, it will call the equals method of the parameters if the parameters are primitive double, it will tranfer the double paramters to long bytes  and then compare

--- welcode/src/testcase/arrays/ComparingArrays.java
+++ welcode/src/testcase/arrays/ComparingArrays.java
@@ -1,8 +1,15 @@
+package testcase.arrays;
+
 //: arrays/ComparingArrays.java
 // Using Arrays.equals()
 import java.util.*;
-import static net.mindview.util.Print.*;
-
+import static testcase.net.mindview.util.Print.*;
+/*
+ * As for Arrrays.equals,there are many overloaded versions, if the parameters are
+ * Object type, it will call the equals method of the parameters
+ * if the parameters are primitive double, it will tranfer the double paramters to long bytes
+ * and then compare
+ */
 public class ComparingArrays {
   public static void main(String[] args) {
     int[] a1 = new int[10];
