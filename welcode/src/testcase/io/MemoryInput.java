287b0ef9b1d3038123a89548eb3e5f66b59deeaf
Welmanhotheart <a274936580@163.com>
2018-04-28 15:10:45 +0800

    io|MemoryInput.java: StringReader read one char at one time

--- welcode/src/testcase/io/MemoryInput.java
+++ welcode/src/testcase/io/MemoryInput.java
@@ -1,11 +1,16 @@
+package testcase.io;
+
 //: io/MemoryInput.java
 import java.io.*;
 
 public class MemoryInput {
   public static void main(String[] args)
   throws IOException {
+  	/*
+  	 * read one char at one time
+  	 */
     StringReader in = new StringReader(
-      BufferedInputFile.read("MemoryInput.java"));
+      BufferedInputFile.read("D:/learnjavaworkspace/TheReplacements.txt"));
     int c;
     while((c = in.read()) != -1)
       System.out.print((char)c);
