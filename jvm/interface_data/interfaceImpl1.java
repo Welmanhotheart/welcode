686e52a23bd44b1141ae7765a281abb33253bdf9
weizhiwei <weizhiwei@111.com.cn>
2018-11-30 11:16:10 +0800

    字段表集合总结

--- jvm/interface_data/interfaceImpl1.java
+++ jvm/interface_data/interfaceImpl1.java
@@ -1,6 +1,6 @@
 package interface_data;
 
-public class interfaceImpl1 implements interface_1 {
+public class interfaceImpl1 implements interface_1,Interface_2 {
 
     public int add(int i, int j) {
         return i+j;
--- jvm/interface_data/interface_1.java
+++ jvm/interface_data/interface_1.java
@@ -1,5 +1,5 @@
 package interface_data;
 
-public interface interface_1 {
+public interface interface_1 extends Interface_f_1{
     int add(int i, int j);
 }
