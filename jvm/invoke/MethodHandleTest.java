55d709ac8e8f8941bac42df1d167d786cff2a6bf
weizhiwei <weizhiwei@111.com.cn>
2018-12-14 09:29:51 +0800

    自主实验学习

--- jvm/invoke/MethodHandleTest.java
+++ jvm/invoke/MethodHandleTest.java
@@ -29,6 +29,38 @@ public class MethodHandleTest {
         /*lookup()方法来自于MethodHandles.lookup,这句的作用是在指定类中查找符合给定的方法名称、 方法类型,并且符合调用权限的方法句柄*/
 /*因为这里调用的是一个虚方法,按照Java语言的规则,方法第一个参数是隐式的,代表该方法的接收者,也即是this指向的对象,这个参数以前是放在参数列表中进行传递的,而现在提供了bindTo()方法来完
 成这件事情*/
-        return lookup().findVirtual(reveiver.getClass(),"println",mt).bindTo(reveiver);
+        return lookup().findVirtual(reveiver.getClass(), "println", mt).bindTo(reveiver);
     }
 }
+
+
+class Test {
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                MethodHandle mh = lookup().findSpecial(GrandFather.class, "thinking", mt, getClass());
+                mh.invoke(this);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        (new Test().new Son()).thinking();
+    }
+}
+
