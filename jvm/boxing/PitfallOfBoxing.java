df0e551a6df22a76217c4f19ca86e13dca16251c
weizhiwei <weizhiwei@111.com.cn>
2019-01-18 13:35:57 +0800

    导入openjdk源码

--- jvm/boxing/PitfallOfBoxing.java
+++ jvm/boxing/PitfallOfBoxing.java
@@ -9,11 +9,11 @@ public class PitfallOfBoxing {
         Integer e=321;
         Integer f=321;
         Long g=3L;
-        System.out.println(c==d);
-        System.out.println(e==f);
-        System.out.println(c==(a+b));
-        System.out.println(c.equals(a+b));
-        System.out.println(g==(a+b));
-        System.out.println(g.equals(a+b));
+        System.out.println(c==d);//true
+        System.out.println(e==f);//false
+        System.out.println(c==(a+b));//true
+        System.out.println(c.equals(a+b));//true
+        System.out.println(g==(a+b));//true
+        System.out.println(g.equals(a+b));//false
     }
 }
