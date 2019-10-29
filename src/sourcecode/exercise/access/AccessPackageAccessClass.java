c41163adeb99cd568c98e5dd287c0650545c0c80
Welmanhotheart <a274936580@163.com>
2019-10-29 11:02:13 +0800

    包转移

--- src/sourcecode/exercise/access/AccessPackageAccessClass.java
+++
@@ -1,9 +0,0 @@
-package exercise.access;
-
-import exercise.access.accesspackage.*;
-
-public class AccessPackageAccessClass {
-    public static void main(String[] args) {
-//        new classWithPackageAccess();
-    }
-}
