b8acdd45acc1fac68a134c0df75979bd3d7107e1
Welmanhotheart <a274936580@163.com>
2019-10-31 17:51:07 +0800

    String.intern再次学习

--- jvm/memory/RuntimeConstantPoolOOM.java
+++ jvm/memory/RuntimeConstantPoolOOM.java
@@ -10,15 +10,46 @@ public class RuntimeConstantPoolOOM {
 //		while (true) {
 //			list.add(String.valueOf(i++).intern());
 //		}
-//		String str1 = new StringBuilder("¼ÆËã»ú").append("Èí¼þ").toString();
+//		String str1 = new StringBuilder("ï¿½ï¿½ï¿½ï¿½ï¿½").append("ï¿½ï¿½ï¿½").toString();
 //		System.out.println(str1.intern() == str1);
 		
 //		String str1 = new StringBuilder("rt").append("vs").toString();
 //		System.out.println(str1.intern() == str1);
 		
-		String str2 = new StringBuilder("ja").append("va").toString();
-		System.out.println(str2.intern() == str2);
-		
+//		String str2 = new StringBuilder("ja").append("va").toString();
+//		System.out.println(str2.intern() == str2);
+
+
+//		String str1 = "todo";
+//		String str2 = "todo";
+//		String str3 = "to";
+//		String str4 = "do";
+//		String str5 = str3 + str4;
+//		String str6 = new String(str1);
+//
+//		System.out.println("------æ™®é€šStringæµ‹è¯•ç»“æžœ------");
+//		System.out.print("str1 == str2 ? ");
+//		System.out.println( str1 == str2);
+//		System.out.print("str1 == str5 ? ");
+//		System.out.println(str1 == str5);
+//		System.out.print("str1 == str6 ? ");
+//		System.out.print(str1 == str6);
+//		System.out.println();
+//
+//		System.out.println("---------internæµ‹è¯•ç»“æžœ---------");
+//		System.out.print("str1.intern() == str2.intern() ? ");
+//		System.out.println(str1.intern() == str2.intern());
+//		System.out.print("str1.intern() == str5.intern() ? ");
+//		System.out.println(str1.intern() == str5.intern());
+//		System.out.print("str1.intern() == str6.intern() ? ");
+//		System.out.println(str1.intern() == str6.intern());
+//		System.out.print("str1 == str6.intern() ? ");
+//		System.out.println(str1 == str6.intern());
+
+		String a = "abc";
+//		String b = new String("abc");
+		System.out.println(a == "abc");
+		System.out.println(a == a.intern());
 	}
 	
 
