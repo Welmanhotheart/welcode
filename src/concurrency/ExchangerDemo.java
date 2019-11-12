6d25e7d15ad767738f7d764c90e641fad2d58a06
Welmanhotheart <a274936580@163.com>
2019-11-12 10:57:50 +0800

    验证exchange的交换功能

--- src/concurrency/ExchangerDemo.java
+++ src/concurrency/ExchangerDemo.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 import net.mindview.util.*;
 
+import static net.mindview.util.Print.print;
+
 class ExchangerProducer<T> implements Runnable {
     private Generator<T> generator;
     private Exchanger<List<T>> exchanger;
@@ -23,10 +25,15 @@ class ExchangerProducer<T> implements Runnable {
                 for (int i = 0; i < ExchangerDemo.size; i++)
                     holder.add(generator.next());
                 // Exchange full for empty:
-                holder = exchanger.exchange(holder);
+                print("waiting for exchange");
+                holder = exchanger.exchange(holder, 1, TimeUnit.SECONDS);
+                print("finished exchanging");
+                System.out.println("in ExchangerProducer:" + holder);
             }
         } catch (InterruptedException e) {
             // OK to terminate this way.
+        } catch (TimeoutException e) {
+//            e.printStackTrace();
         }
     }
 }
@@ -45,6 +52,7 @@ class ExchangerConsumer<T> implements Runnable {
         try {
             while (!Thread.interrupted()) {
                 holder = exchanger.exchange(holder);
+                System.out.println("in ExchangerConsumer:" + holder);
                 for (T x : holder) {
                     value = x; // Fetch out value
                     holder.remove(x); // OK for CopyOnWriteArrayList
@@ -69,8 +77,12 @@ public class ExchangerDemo {
         ExecutorService exec = Executors.newCachedThreadPool();
         Exchanger<List<Fat>> xc = new Exchanger<List<Fat>>();
         List<Fat>
-                producerList = new CopyOnWriteArrayList<Fat>(),
-                consumerList = new CopyOnWriteArrayList<Fat>();
+                producerList = new MyCopyOnWriteArrayList<Fat>(),
+                consumerList = new MyCopyOnWriteArrayList<Fat>();
+        System.out.println("producerList,in main:" + producerList);
+        System.out.println("consumerList,in main:" + consumerList);
+        print();
+        print();
         exec.execute(new ExchangerProducer<Fat>(xc,
                 BasicGenerator.create(Fat.class), producerList));
         exec.execute(
@@ -78,6 +90,47 @@ public class ExchangerDemo {
         TimeUnit.SECONDS.sleep(delay);
         exec.shutdownNow();
     }
-} /* Output: (Sample)
+}
+
+
+class MyCopyOnWriteArrayList<E> extends CopyOnWriteArrayList<E> {
+    private String hashCode;
+    public MyCopyOnWriteArrayList() {
+        super();
+        this.hashCode = RandomCharacterGenerizor.generize(12);
+    }
+
+    @Override
+    public String toString() {
+        return hashCode;
+    }
+
+}
+
+//class MyExchanger extends Exchanger {
+//    private int sleepTime;
+//    private MyExchanger(int sleepTime) {
+//        this.sleepTime = sleepTime;
+//    }
+//
+//    @Override
+//    public Object exchange(Object x, long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
+//        return super.exchange(x, timeout, unit);
+//    }
+//}
+
+class RandomCharacterGenerizor {
+    public static final String letterChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String generize(int length) {
+        StringBuffer sb = new StringBuffer();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(letterChar.charAt(random.nextInt(letterChar.length())));
+        }
+        return sb.toString();
+    }
+}
+
+/* Output: (Sample)
 Final value: Fat id: 29999
 *///:~
