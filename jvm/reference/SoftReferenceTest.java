318adfb29ebf0faaee402a54c031f190c1a62791
Welmanhotheart <a274936580@163.com>
2019-11-20 18:15:15 +0800

    软引用复习

--- jvm/reference/SoftReferenceTest.java
+++ jvm/reference/SoftReferenceTest.java
@@ -9,19 +9,19 @@ public class SoftReferenceTest {
         public String name;
         public BiggerObject(String name){
             this.name=name;
-            values=new int[2048];
+            values=new int[1024 * 1024];
         }
     }
 
     public static void main(String[] args) {
-        int count=100;//对象数量为100000，保证使得堆内存溢出
+        int count=20;//对象数量为100000，保证使得堆内存溢出
         SoftReference[] values=new SoftReference[count];
         for(int i=0;i<count;i++){
             values[i]=new SoftReference<BiggerObject>(new BiggerObject("Object-"+i));
         }
-        System.gc();//，如果内存不足，即使强制进行垃圾回收也会发生内存溢出；如果内存充足，即使强制进行垃圾回收也不会回收对象
+//        System.gc();//，如果内存不足，即使不进行垃圾回收也会发生内存溢出；如果内存充足，即使强制进行垃圾回收也不会回收对象
         System.out.println(((BiggerObject)(values[values.length-1].get())).name);
-        for(int i=0;i<10;i++){
+        for(int i=10;i<1;i++){
             System.out.println(((BiggerObject)(values[i].get())).name);
         }
     }
