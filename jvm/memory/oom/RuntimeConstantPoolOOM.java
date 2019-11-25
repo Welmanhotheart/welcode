8c3eb5b95a81328db45f0ee7233a78d35c2a2a41
Welmanhotheart <a274936580@163.com>
2019-11-25 22:48:53 +0800

    String intern test case

--- jvm/memory/oom/RuntimeConstantPoolOOM.java
+++ jvm/memory/oom/RuntimeConstantPoolOOM.java
@@ -1,32 +1,22 @@
 package memory.oom;
 
-import java.util.concurrent.TimeUnit;
-
 public class RuntimeConstantPoolOOM {
-	public static void main(String[] args) throws InterruptedException {
-//		List<String> list = new ArrayList<String>();
-//		int i = 0;
-//		while (true) {
-//			list.add(String.valueOf(i++).intern());
-//		}
-//		String str1 = new StringBuilder("�����").append("���").toString();
-//		System.out.println(str1.intern() == str1);
-		
-//		String str1 = new StringBuilder("rt").append("vs").toString();
-//		System.out.println(str1.intern() == str1);
-		
-		String str2 = new StringBuilder("ja").append("va").toString();
-		System.out.println("str2.intern() == str2 " + (str2.intern() == str2));
-		String str3 = "1234";
-		System.out.println("str3.intern() == str3 " + (str3.intern() == str3));
-		String str4 = new String("123");
-		System.out.println("str4.intern() == str4 " + (str4.intern() == str4));
+	public static void main(String[] args) {
 
-		String str5 = "你好的";
-		System.out.println("str5.intern() == str5 " + (str5.intern() == str5));
+		String str1 = new StringBuilder("bc").append("dc").toString( ) ;
+		System.out.println(str1.intern() == str1) ;
 
-		TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+		String str3 =new StringBuilder( "计算机").append( "软件").toString( ) ;
+		System.out.println(str3.intern() == str3) ;
+
+		String str2 = new StringBuilder("ja").append("va").toString( ) ;
+		System.out.println(str2.intern() == str2) ;
 	}
-	
 
 }
+/**
+ * output:
+ * true
+  *false
+  */
+
