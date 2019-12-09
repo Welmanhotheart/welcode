66858310ba5afec988edb2ca455f7b8ed396d269
Welmanhotheart <a274936580@163.com>
2019-12-09 16:09:21 +0800

    when pull in messages manually, invocation of message.acknowledge() is needed

---
+++ activemq/src/main/java/com/wei/activemq/queue/pullin/JmsConsumer_Pullin.java
@@ -0,0 +1,49 @@
+package com.wei.activemq.queue.pullin;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class JmsConsumer_Pullin {
+    private static final String ACTIVEMQ_URL = "tcp://192.168.73.130:61616";
+    private static final String QUEUE_NAME  = "queue-producer";
+    public static void main(String[] args) throws JMSException, IOException {
+
+        System.out.println("I am the second consumer");
+        // create connection factory
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+
+        factory.setTrustedPackages(Arrays.asList("com.wei.activemq.beans"));
+        // through connection factory, get connection and 'start'
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        // create session and
+
+        //parameter1: 1.transaction  2.check in
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+        // create destination , theme or queue
+        Queue queue = session.createQueue(QUEUE_NAME);
+
+
+        // create consumer
+        MessageConsumer consumer = session.createConsumer(queue);
+
+        while(true) {
+            Message message = consumer.receive(4000l);
+            if (message != null && message instanceof  TextMessage) {
+                System.out.println(((TextMessage) message).getText());
+                message.acknowledge();
+            } else {
+                break;
+            }
+        }
+//        session.commit();
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+}
