3cda997dfb515135c95fc5b11f7e47ba236a7d11
Welmanhotheart <a274936580@163.com>
2019-12-09 09:23:34 +0800

    activemq-sgg

---
+++ activemq/src/main/java/com/wei/activemq/queue/JmsProduce.java
@@ -0,0 +1,37 @@
+package com.wei.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class JmsProduce {
+    private static final String ACTIVEMQ_URL = "tcp://192.168.73.130:61616";
+    private static final String QUEUE_NAME  = "queue-producer";
+    public static void main(String[] args) throws JMSException {
+
+        // create connection factory
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+
+        // through connection factory, get connection and 'start'
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        // create session and
+
+        //parameter1: 1.transaction  2.check in
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // create destination , theme or queue
+        Queue queue = session.createQueue(QUEUE_NAME);
+
+        MessageProducer producer = session.createProducer(queue);
+        for(int i = 0; i < 6; i++) {
+            TextMessage textMessage = session.createTextMessage("message-" + i);
+            producer.send(textMessage);
+        }
+        System.out.println("message sent over");
+        producer.close();
+        session.close();
+        connection.close();
+    }
+}
---
+++ activemq/src/main/java/com/wei/activemq/queue/JmsProduce_Topic.java
@@ -0,0 +1,37 @@
+package com.wei.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+
+public class JmsProduce_Topic {
+    private static final String ACTIVEMQ_URL = "tcp://192.168.73.130:61616";
+    private static final String TOPIC_NAME  = "topic-producer";
+    public static void main(String[] args) throws JMSException, JMSException {
+        // create connection factory
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+
+        // through connection factory, get connection and 'start'
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        // create session and
+
+        //parameter1: 1.transaction  2.check in
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // create destination , theme or queue
+        Topic topic = session.createTopic(TOPIC_NAME);
+
+        MessageProducer producer = session.createProducer(topic);
+        for(int i = 0; i < 6; i++) {
+            TextMessage textMessage = session.createTextMessage("message-" + i);
+            producer.send(textMessage);
+        }
+        System.out.println("message sent over");
+        producer.close();
+        session.close();
+        connection.close();
+    }
+}
