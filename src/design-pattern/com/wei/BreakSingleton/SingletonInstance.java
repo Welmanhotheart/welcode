b90791b2e0bb4bcb750157c6b81bd8dce622d8f4
Welmanhotheart <a274936580@163.com>
2019-12-21 16:05:22 +0800

    关于单例模式及其破坏方式，及阻止其破坏的问题研究

---
+++ src/design-pattern/com/wei/BreakSingleton/SingletonInstance.java
@@ -0,0 +1,34 @@
+package com.wei.BreakSingleton;
+
+import java.io.Serializable;
+
+public class SingletonInstance  implements Serializable  {
+    private static final long serialVersionUID = -7327434548808172995L;
+
+    private static volatile SingletonInstance  instance;
+    private static boolean flag = true;
+    private SingletonInstance() {
+        if(flag) {
+            System.out.println(" SingletonInstance build singleton the first time");
+            flag = false;
+        } else {
+            throw new RuntimeException("single pattern invaded, " +
+                    "second instance unsuccessfully constructed");
+        }
+    }
+    public static SingletonInstance getInstance() {
+        if(instance == null) {
+            synchronized (SingletonInstance.class) {
+                if (instance == null) {
+                    instance = new SingletonInstance();
+                }
+            }
+        }
+        return instance;
+    }
+
+    //
+    private  Object readResolve() {
+        return instance;
+    }
+}
