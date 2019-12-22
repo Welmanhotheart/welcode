b67653105e54e5b42f8eb936193e6ee18613af46
Welmanhotheart <a274936580@163.com>
2019-12-22 19:44:02 +0800

    Constructor of enum must be package or private access, couldn't be public or protected

--- src/enumerated/OzWitch.java
+++ src/enumerated/OzWitch.java
@@ -1,4 +1,4 @@
-//: enumerated/OzWitch.java
+package enumerated;//: enumerated/OzWitch.java
 // The witches in the land of Oz.
 
 import static net.mindview.util.Print.*;
@@ -9,10 +9,19 @@ public enum OzWitch {
     NORTH("Glinda, the Good Witch of the North"),
     EAST("Wicked Witch of the East, wearer of the Ruby " +
             "Slippers, crushed by Dorothy's house"),
-    SOUTH("Good by inference, but missing");
+    SOUTH("Good by inference, but missing");//Here it's force to execute the constructor with arguments
     private String description;
 
-    // Constructor must be package or private access:
+    /**
+     *  Constructor must be package or private access:
+     *  constructor couldn't be public or protected.
+     *  And through debugging, It can be shown that before executing the constructor below,
+     *  constructor of Enum class here shown below must be executed as the first
+     *  protected Enum(String name, int ordinal) {
+     *    this.name = name;
+     *    this.ordinal = ordinal;
+     * }
+    */
     private OzWitch(String description) {
         this.description = description;
     }
@@ -23,7 +32,7 @@ public enum OzWitch {
 
     public static void main(String[] args) {
         for (OzWitch witch : OzWitch.values())
-            print(witch + ": " + witch.getDescription());
+            print(witch.name() + ": " + witch.getDescription());
     }
 } /* Output:
 WEST: Miss Gulch, aka the Wicked Witch of the West
