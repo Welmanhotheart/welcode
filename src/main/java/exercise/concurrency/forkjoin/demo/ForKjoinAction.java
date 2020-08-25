3069ac444290d87c60e43104aae1676d70883aa9
weizhiwei <zhiwei.wei@bintools.cn>
2020-08-25 18:08:56 +0800

    forkjoin代码测试完善

--- src/main/java/exercise/concurrency/forkjoin/demo/ForKjoinAction.java
+++ src/main/java/exercise/concurrency/forkjoin/demo/ForKjoinAction.java
@@ -1,10 +1,16 @@
 package exercise.concurrency.forkjoin.demo;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 
 
 public class ForKjoinAction extends RecursiveAction {
+
+
     /**
      * 包含startIndex
      */
@@ -30,15 +36,13 @@ public class ForKjoinAction extends RecursiveAction {
         } else {
             int middle = (startIndex + endIndex)/2;
             ForKjoinAction left = new ForKjoinAction(startIndex, middle, arr);
-            left.fork();
             ForKjoinAction right = new ForKjoinAction(middle, endIndex, arr);
-            right.fork();
 
-            left.join();right.join();
+            ForKjoinAction.invokeAll(left, right);
+            left.join(); right.join();
 
             /**这里做归并工作，后续形成自己的工具类**/
             mergeSort(arr, startIndex, middle, endIndex);
-
         }
     }
 
@@ -49,7 +53,62 @@ public class ForKjoinAction extends RecursiveAction {
      * @param middle
      * @param endIndex
      */
-    private static void mergeSort(int[] arr, int startIndex, int middle, int endIndex) {
+    private static void mergeSort(int[] arr, final int startIndex,
+                                  final int middle, final int endIndex) {
+        int start1 = startIndex;
+        int start2 = middle;
 
+        int[] tmp = new int[endIndex - startIndex];
+        int count = 0;
+        while(start1 < middle && start2 < endIndex) {
+            if ( arr[start1] < arr[start2]) {
+                tmp[count++] = arr[start1++];
+            } else {
+                tmp[count++] = arr[start2++];
+            }
+        }
+
+        int leftStart = -1;
+        int leftEnd = -1;
+        if (start1 < middle) {
+            leftStart = start1;
+            leftEnd = middle;
+        } else {
+            leftStart = start2;
+            leftEnd = endIndex;
+        }
+
+        for (int i = leftStart; i < leftEnd; i++) {
+            tmp[count++] = arr[i];
+        }
+        //复制
+
+        for (int i = startIndex, j=0; i < endIndex; i++,j++) {
+            arr[i] = tmp[j];
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //左开右闭
+        int a[] = {2, 45, 67, 13, 13, 24, 57};
+        mergeSort(a,0, 3, 7);
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+
+        int len = 10000000;
+        a = new int[len];
+        Random r = new Random(10000);
+        for (int i = 0; i < len; i++) {
+            a[i] = r.nextInt(10000);
+        }
+
+        long start = System.currentTimeMillis();
+        ForKjoinAction forKjoinAction = new ForKjoinAction(0, len, a);
+        ForkJoinPool pool = ForkJoinPool.commonPool();
+        ForkJoinTask<Void> submit = pool.submit(forKjoinAction);
+        submit.get();
+        System.out.println("I am done, time consumed:" + (System.currentTimeMillis() - start));
     }
 }
