fedba8ed0539bdc6db2fd0636242e45815c7e249
weizhiwei <zhiwei.wei@bintools.cn>
2022-07-07 14:57:11 +0800

    注释掉部分代码。

---
+++ think-in-java/src/main/java/concurrency/TestLinkedBlockingQueue.java
@@ -0,0 +1,75 @@
+package concurrency;
+
+import java.util.concurrent.*;
+
+public class TestLinkedBlockingQueue {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(50);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() / 3);
+        ForkJoinTask<Integer> producer = forkJoinPool.submit(new TestLinkedBlockingQueueTaskProducer(queue));
+//        ForkJoinTask<Integer> consumer = forkJoinPool.submit(new TestLinkedBlockingQueueTaskConsumer(queue));
+        producer.get();
+//        consumer.get();
+        forkJoinPool.shutdown();
+    }
+
+}
+
+
+class TestLinkedBlockingQueueTaskProducer extends RecursiveTask<Integer> {
+    private LinkedBlockingQueue<Integer> queue;
+
+    public TestLinkedBlockingQueueTaskProducer(LinkedBlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    protected Integer compute() {
+        while (!Thread.interrupted()) {
+            Integer take = null;
+            try {
+                for (int i = 0; i < 100; i++) {
+                     queue.put(i);
+                    System.out.println("add: " + (i+1));
+                }
+            } catch (InterruptedException e) {
+
+            }
+            System.out.println("take:" + take);
+        }
+        try {
+            queue.put(-1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+}
+
+
+class TestLinkedBlockingQueueTaskConsumer extends RecursiveTask<Integer> {
+    private LinkedBlockingQueue<Integer> queue;
+
+    public TestLinkedBlockingQueueTaskConsumer(LinkedBlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+
+    @Override
+    protected Integer compute() {
+        while (!Thread.interrupted()) {
+            Integer take = null;
+            try {
+                take = queue.take();
+                if (take < 0) {
+                    break;
+                }
+                System.out.println("take:" + take);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        return null;
+    }
+}
